093d0b0f4411f2b07543d6e6527aedbfe2e088de
Hector Castro <hectcastro@gmail.com>
2015-09-14 08:42:54 -0400

    Ensure Leiningen plugin for Midje is in project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "An automated test suite for the OpenTreeMap website."
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
